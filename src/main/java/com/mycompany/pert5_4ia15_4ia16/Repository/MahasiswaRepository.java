d9c4ea9d79a8f0c5a73f1f2b0fe4ed7d3881e806
Satya Bara Justitia <satyabara25@gmail.com>
2025-11-25 14:35:03 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pert5_4ia15_4ia16/Repository/MahasiswaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert5_4ia15_4ia16.Repository;
+
+import com.mycompany.pert5_4ia15_4ia16.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author BARA
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Long> {
+
+}
